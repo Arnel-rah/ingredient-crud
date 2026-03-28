8ed34a29513a61e2e5d1ea40dfea8dc4d7159d2d
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-28 19:51:55 +0300

    refactor: rename packages to lowercase for SRP compliance

--- src/main/java/hei/school/ingredientcrud/Exceptions/BadRequestExceptions.java
+++
@@ -1,7 +0,0 @@
-package hei.school.ingredientcrud.exceptions;
-
-public class BadRequestExceptions extends Exception{
-  public BadRequestExceptions(String message) {
-    super(message);
-  }
-}
--- src/main/java/hei/school/ingredientcrud/entity/CategoryIngredient.java
+++
@@ -1,5 +0,0 @@
-package hei.school.ingredientcrud.entity;
-
-public enum CategoryIngredient {
-  VEGETABLE, ANIMAL, DAIRY, OTHER
-}
